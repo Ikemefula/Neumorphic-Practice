679cd075cc461de2cc44c98acdb4fa969252146e
Owt And About <70513244+Ikemefula@users.noreply.github.com>
2021-12-16 20:52:45 -0600

    updated

--- neumorphic-practice-react/src/App.js
+++ neumorphic-practice-react/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+import logo from './WORLD CINEMA LOGO.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="World Cinema logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <p>
+          I am attemptting to convert an html page into react code. 
         </p>
         <a
           className="App-link"
@@ -17,7 +21,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
+        <a
+          className="App-link"
+          href="http://127.0.0.1:5500/HTML/DARK%20THEME/index.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check out my Neumorphic themed Resume here!
+        </a>      
+        </header>
     </div>
   );
 }
